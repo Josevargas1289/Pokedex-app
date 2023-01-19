a927f0b0ea187a398cd943f11d3accf031e65ad3
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-19 13:46:19 -0500

    avances pantall apokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Pokedex = () => {
+
+
+
+
     const Username = useSelector((state) => state.Username)
+    const navigate = useNavigate();
+
+    const backPage = () => {
+        navigate(-1)
+    }
     return (
-        <div>
-            <h1>Pokedex</h1>
-            <h3><b>{Username}</b></h3>
+        <div className='container-Pokedex'>
+            <div className='pokedex'>
+                <h1 className='title-pokedex'>Pokedex</h1>
+                <h3 className='subtitle-pokedex'>Welcome <b className='name'>{Username}</b>, here you can find your favorite pokemon</h3>
+                <button onClick={backPage} className='btn-back'><i class='bx bx-left-arrow-circle bx-lg'></i></button>
+            </div>
         </div>
     );
 };
