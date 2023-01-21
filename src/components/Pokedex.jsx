e31951de56fe239df05d716f17539f189bf94dd0
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-21 13:07:15 -0500

    lista la paginacion

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -13,19 +13,31 @@ const Pokedex = () => {
     const navigate = useNavigate();
     const [pokemons, setPokemons] = useState([]);
     const [inputSearch, setInputSearch] = useState('')
-    const [pokemoType, setPokemonType]= useState([]);
-    
+    const [pokemoType, setPokemonType] = useState([]);
+
 
     useEffect(() => {
         axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
             .then((res) => setPokemons(res.data.results))
 
         axios.get('https://pokeapi.co/api/v2/type')
-        .then((res)=> setPokemonType(res.data.results))
+            .then((res) => setPokemonType(res.data.results))
 
 
 
     }, []);
+    const [page, setPage] = useState(1);
+    const pokemonsPerpage = 9;
+    const lastIndex = page * pokemonsPerpage;
+    const firstIndex = lastIndex - pokemonsPerpage;
+    const pokemonPaginated = pokemons.slice(firstIndex, lastIndex)
+    const totalPages = Math.ceil(pokemons.length / pokemonsPerpage)
+
+    const pages = []
+    for(let i = 1; i<= totalPages; i++){
+        pages.push(i)
+    }
+
     console.log(pokemoType);
 
     const backPage = () => {
@@ -33,13 +45,15 @@ const Pokedex = () => {
 
     }
 
-    const search = ()=>{
+    const search = () => {
         navigate(`/pokedex/${inputSearch.toLowerCase()}`)
-       }
-       const filterType = (e)=>{
+    }
+    const filterType = (e) => {
         axios.get(e.target.value)
-        .then((res) =>setPokemons(res.data.pokemon))
-       }
+            .then((res) => setPokemons(res.data.pokemon))
+    }
+
+   
 
     return (
 
@@ -52,29 +66,29 @@ const Pokedex = () => {
                 <h3 className='subtitle-pokedex'>Welcome <b className='name'>{Username}</b>, here you can find your favorite pokemon</h3>
                 <br />
                 <div className='container-search'>
-                <input type="text"
-                placeholder='search pokemon'
-                value={inputSearch} 
-                onChange={(e)=> setInputSearch(e.target.value)}
-                />
-                <button className='btn-search' onClick={search}>Search</button>
-              
+                    <input type="text"
+                        placeholder='search pokemon'
+                        value={inputSearch}
+                        onChange={(e) => setInputSearch(e.target.value)}
+                    />
+                    <button className='btn-search' onClick={search}>Search</button>
 
-                <select onChange={filterType} name="" id="">
-                    {
-                        pokemoType.map((type)=>(
-                            <option value ={type.url} key={type.url}>
-                                {type.name}
-                            </option>
 
-                        ))
-                    }
-                  
-                </select>
+                    <select onChange={filterType} name="" id="">
+                        {
+                            pokemoType.map((type) => (
+                                <option value={type.url} key={type.url}>
+                                    {type.name}
+                                </option>
+
+                            ))
+                        }
+
+                    </select>
                 </div>
                 <ul className='grid'>
                     {
-                        pokemons.map(pokemon => (
+                        pokemonPaginated?.map(pokemon => (
                             <PokemonCard
                                 key={pokemon.url ? pokemon.url : pokemon.pokemon.url}
                                 url={pokemon.url ? pokemon.url : pokemon.pokemon.url}
@@ -83,6 +97,28 @@ const Pokedex = () => {
                     }
                 </ul>
                 <button onClick={backPage} className='btn-back'><i className='bx bx-left-arrow-circle bx-sm'></i></button>
+            </div>
+            <div className='pagination'>
+                <button onClick={() => setPage(page - 1)} className='pagination-btn'
+                    disabled={page === 1}
+                    
+                ><i className='bx bxs-chevrons-left bx-md'></i>
+                </button>
+                {/* {page} */}
+                {pages.map(number =>
+                    <button
+                   
+                    className='number-pages'
+                        onClick={() => setPage(number)}>{number}
+
+                    </button>)}
+
+                {/* {totalPages} */}
+                <button onClick={() => setPage(page +1)} className='pagination-btn'
+                    disabled={page === totalPages}
+                ><i className='bx bxs-chevrons-right bx-md'></i>
+                </button>
+
             </div>
         </div>
     );
