1dc4c2b011e5300ef22f42def4e677da3503c856
JOSE VARGAS¡¡ <joseroberto-vargas@hotmail.com>
2023-01-21 21:34:58 -0500

    stylando el input-name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { HashRouter, Route, Routes} from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Inputname from './components/Inputname'
 import Pokedex from './components/Pokedex'
 import PokedexId from './components/PokedexId'
@@ -16,11 +16,11 @@ function App() {
 
   const [isLoading, setIsLoading] = useState(true);
   const [switchMode, setSwitchMode] = useState(false);
-  const [darkmode,setDarkmode] = useState(false)
-  
-  const load =()=>{
+  const [darkmode, setDarkmode] = useState(false)
+
+  const load = () => {
     setTimeout(() => setIsLoading(false), 1000);
-    
+
   }
   load();
 
@@ -34,54 +34,54 @@ function App() {
   }
 
   return (
-    <div className= 
-    {
-      `container-app ${darkmode ? 'dark': 'ligth'}`
-    }
-    
-    >
-      
-    <HashRouter>
-      <header style={{display: isLoading ? 'none' : 'block'}}>
-      <button
-              onClick={switchOn}
-              className={`switch ${classDark}`}
-              id="swith">
-              <span>
-                <i className="bx bx-sun bx-sm"></i>
-              </span>
-              <span>
-                <i className="bx bx-moon bx-sm"></i>
-              </span>
-            </button>
-      </header>
-    
+    <div className=
       {
-        isLoading ? (
-          <div className='loader'>
-            <img src={pokeball} alt="" />
-            <h1 className='animate__animated animate__bounce animate__heartBeat'>Cargando Pokedex...</h1>
-          
-          </div>
-          
-
-        ) : (
-         
-          
-          <Routes>
-       
-            <Route path='/'element={<Inputname />} />
-            <Route element={<ProtectedRoutes/>}>
-            <Route path='/pokedex' element={<Pokedex/>} />
-            <Route path='/pokedex/:id' element={<PokedexId />} />
-
-            </Route>
-          </Routes>
-
-        )
+        `container-app ${darkmode ? 'dark' : 'ligth'}`
       }
-    
-    </HashRouter>
+
+    >
+
+      <HashRouter>
+        <header style={{ display: isLoading ? 'none' : 'block' }}>
+          <button
+            onClick={switchOn}
+            className={`switch ${classDark}`}
+            id="swith">
+            <span>
+              <i className="bx bx-sun bx-sm"></i>
+            </span>
+            <span>
+              <i className="bx bx-moon bx-sm"></i>
+            </span>
+          </button>
+        </header>
+
+        {
+          isLoading ? (
+            <div className='loader'>
+              <img src={pokeball} alt="" />
+              <h1 className='animate__animated animate__bounce animate__heartBeat'>Cargando Pokedex...</h1>
+
+            </div>
+
+
+          ) : (
+
+
+            <Routes>
+
+              <Route path='/' element={<Inputname />} />
+              <Route element={<ProtectedRoutes />}>
+                <Route path='/pokedex' element={<Pokedex />} />
+                <Route path='/pokedex/:id' element={<PokedexId />} />
+
+              </Route>
+            </Routes>
+
+          )
+        }
+
+      </HashRouter>
     </div>
   )
 }
